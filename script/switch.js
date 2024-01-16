02b643c883edc147d51c6e536529a5e4e1fdb88f
Rick <patrick@yesthisispatrick.com>
2024-01-15 17:40:27 -0700

    Add focus to the resume

---
+++ script/switch.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let myBtns = document.querySelectorAll('.btn-group button');
+    myBtns.forEach(function(btn) {
+        btn.addEventListener('click', () => {
+          myBtns.forEach(b => b.classList.remove('clicked'))
+          btn.classList.add('clicked')
+        })
+    })
+})
+
+function hideRoles(role) {
+  showAll()
+  Array.from(document.getElementsByClassName(role))
+    .forEach(e => e.classList.add('hide'))
+  this.classList.add('clicked')
+}
+
+function showAll() {
+  Array.from(document.getElementsByClassName('hide'))
+    .forEach(e => e.classList.remove('hide'))
+
+  Array.from(document.getElementsByClassName('clicked'))
+    .forEach(e => e.classList.remove('clicked'))
+
+}
